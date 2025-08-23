391933fe0254694580c7ac566d3f5de72c46a1db
Hermann <hermanhigalgoa@gmail.com>
2025-08-23 15:46:55 -0600

    Usuario y Medico

--- src/main/java/hospital/logic/Medico.java
+++ src/main/java/hospital/logic/Medico.java
@@ -1,11 +1,9 @@
 package hospital.logic;
 import java.util.Objects;
 
-public class Medico {
-    String id;
-    String clave;
-    String nombre;
-    String especialidad;
+public class Medico extends Usuario {
+    private String clave;
+    private String especialidad;
 
     public Medico() {this("","","","");}
 
@@ -15,15 +13,11 @@ public class Medico {
         this.nombre = nombre;
         this.especialidad = especialidad;
     }
-    public String getId() { return id; }
     public String getClave() { return clave; }
-    public String getNombre() { return nombre; }
     public String getEspecialidad() { return especialidad; }
 
 
-    public void setId(String id) { this.id = id; }
     public void setClave(String clave) { this.clave = clave; }
-    public void setNombre(String nombre) { this.nombre = nombre; }
     public void setEspecialidad(String especialidad) { this.especialidad = especialidad; }
 
 }
---
+++ src/main/java/hospital/logic/Usuario.java
@@ -0,0 +1,23 @@
+package hospital.logic;
+
+public abstract class Usuario {
+    protected String id;
+    protected String nombre;
+
+
+    public Usuario() {
+
+    }
+
+    public Usuario(String id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+    public String getId() {return id;}
+    public void setId(String id) {this.id = id;}
+    public String getNombre() {return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
+
+
+
+}
